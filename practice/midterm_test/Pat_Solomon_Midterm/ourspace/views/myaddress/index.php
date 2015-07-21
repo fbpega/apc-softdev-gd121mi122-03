2c5ad98241bc15fd5a76378c67161cf87c9a6c7f
patsolomon <akosipat0909@yahoo.com>
2015-07-21 13:19:31 +0800

    my address and my comment has been generated

---
+++ practice/midterm_test/Pat_Solomon_Midterm/ourspace/views/myaddress/index.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Myaddresses';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="myaddress-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Myaddress', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'firstname',
+            'middlename',
+            'lastname',
+            'gender',
+            // 'created_at',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
