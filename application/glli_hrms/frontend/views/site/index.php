0821d3c2d9fc20f45bfd211751ecadf88b98cc19
acbalutan <acbalutan@student.apc.edu.ph>
2015-08-05 23:00:34 +0800

    First draft of application without payroll

---
+++ application/glli_hrms/frontend/views/site/index.php
@@ -0,0 +1,25 @@
+<?php
+/* @var $this yii\web\View */
+$this->title = 'Great Leaf HRMS';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+
+        <p><a class="btn btn-lg btn-success">Login!</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <form class="site-signup">
+            <div id="username">
+                <a>Username:</a>
+                <input name="username"></input>
+            </div>
+            <div id="password">
+                <a>Password:</a>
+                <input name="Password:" type="password"></input>
+            </div>
+        </form>
+    </div>
+</div>
