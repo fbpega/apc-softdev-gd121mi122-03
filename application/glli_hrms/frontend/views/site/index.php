eaae325e5ffc0c9d5fa054b489bb8400131313f3
Francis Vab Pega <vabpega@gmail.com>
2015-08-24 22:42:42 +0800

    dtr work in progress

--- application/glli_hrms/frontend/views/site/index.php
+++ application/glli_hrms/frontend/views/site/index.php
@@ -1,27 +1,26 @@
 <?php
 /* @var $this yii\web\View */
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use app\models\LoginDtr;
+
 $this->title = 'GreatLeaf HRM';
 ?>
 <div class="site-index">
     
          <img class="" src="../images/new-logo-v3.png" style="height: 78px; width: 304px; position: absolute; top: 30%;"></img>
 
-         <div class="jumbotron" style="position: absolute; left: 11%; top: 50%;">
-             <p><a class="btn btn-lg btn-success">Login!</a></p>
-         </div>
+         
 
         <div class="body-content">
+            <?php $form = ActiveForm::begin();?>
+            <?= $form->field($model, 'username')->textArea(['rows' => '1'])->label('Username'); ?>
+            <?= $form->field($model, 'password')->textArea(['rows' => '1'])->label('Password'); ?>
 
-            <form style="text-align: center; position: absolute; left: 10%; top: 45%;">
-                <div id="username">
-                       <a>Username:</a>
-                       <input name="username"></input>
-                </div>
-                <div id="password">
-                       <a>Password:&nbsp;</a>
-                       <input name="Password:" type="password"></input>
-                </div>
-            </form>
+            <div class="form-group">
+                <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+            </div>
+            <?php ActiveForm::end(); ?>
         </div>
 </div>
 
@@ -29,8 +28,8 @@ $this->title = 'GreatLeaf HRM';
         <h1>Good day!</h1>
         <h4>Welcome to GreatLeaf Land Human Resources Management System</h4>
         <br>
-        <h5>Use the login form at the left for your Daily Time Record.</h5>
-        <h5>Click system login to access profile, DTR, and form requests.</h5>
+        <h4>Use the login form at the left for your Daily Time Record.</h4>
+        <h4>Click system login to access profile, DTR, and form requests.</h4>
         <br>
-        <h5>If you are having trouble logging in, contact HR system administrator.</h5>
-    </div>
+        <h5>If you are having trouble logging in, contact system administrator.</h5>
+</div>
---
+++ application/glli_hrms/frontend/views/site/logindtr.view.php
@@ -0,0 +1,10 @@
+<?php
+	use yii\helpers\Html;
+?>
+
+<h1>Attendance as of August 24, 2015</h1>
+<p><label>Username</label>:</p>
+	<?= Html::encode($model->username) ?>
+<br /><br />
+<p><label>Password</label>:</p>
+	<?= Html::encode($model->username) ?>
