b380f051b02cec363bf389a2d2c1b30aef4914e6
Patrick Solomon <posolomon@student.apc.edu.ph>
2015-09-09 09:38:44 +0800

    design and employee

---
+++ application/greatleaf/frontend/views/employee/_search.php
@@ -0,0 +1,51 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\EmployeeSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="employee-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'firstname') ?>
+
+    <?= $form->field($model, 'middlename') ?>
+
+    <?= $form->field($model, 'lastname') ?>
+
+    <?= $form->field($model, 'dob') ?>
+
+    <?php // echo $form->field($model, 'age') ?>
+
+    <?php // echo $form->field($model, 'gender') ?>
+
+    <?php // echo $form->field($model, 'email') ?>
+
+    <?php // echo $form->field($model, 'designation') ?>
+
+    <?php // echo $form->field($model, 'department') ?>
+
+    <?php // echo $form->field($model, 'address') ?>
+
+    <?php // echo $form->field($model, 'username') ?>
+
+    <?php // echo $form->field($model, 'password') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
