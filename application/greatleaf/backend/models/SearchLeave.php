8879bc57ff6f533405698e282c7a76bb3c61a295
jpubalde <ubaldepatrick@gmail.com>
2015-09-23 22:52:25 +0800

    Leave model and crud using Gii tool

---
+++ application/greatleaf/backend/models/SearchLeave.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use backend\models\Leave;
+
+/**
+ * SearchLeave represents the model behind the search form about `backend\models\Leave`.
+ */
+class SearchLeave extends Leave
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['filedate', 'reason', 'startdate', 'enddate', 'duration'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Leave::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'filedate' => $this->filedate,
+            'startdate' => $this->startdate,
+            'enddate' => $this->enddate,
+        ]);
+
+        $query->andFilterWhere(['like', 'reason', $this->reason])
+            ->andFilterWhere(['like', 'duration', $this->duration]);
+
+        return $dataProvider;
+    }
+}
---
+++ application/greatleaf/frontend/views/leave/view.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Leave */
+
+$this->title = $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Leaves', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="leave-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'filedate',
+            'reason:ntext',
+            'startdate',
+            'enddate',
+            'duration',
+        ],
+    ]) ?>
+
+</div>
