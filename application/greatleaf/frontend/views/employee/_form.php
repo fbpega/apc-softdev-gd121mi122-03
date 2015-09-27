15934983cd486e4eca1616408681824085f4137e
acbalutan <acbalutan@student.apc.edu.ph>
2015-09-28 03:01:48 +0800

    created Employee model(common), view(both end) controller(both) using Gii

---
+++ application/greatleaf/frontend/views/employee/_form.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Employee */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="employee-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'firstname')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'middlename')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'lastname')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'contact')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'department')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'position')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'gender')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'dob')->textInput() ?>
+
+    <?= $form->field($model, 'request_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
