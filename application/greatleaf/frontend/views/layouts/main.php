7b4842a0997225e4891a75c57e4f7d90b1a9566d
acbalutan <acbalutan@student.apc.edu.ph>
2015-09-28 03:42:13 +0800

    removed about and contact pages from index and created a dropdown list for submit requests links

--- application/greatleaf/frontend/views/layouts/main.php
+++ application/greatleaf/frontend/views/layouts/main.php
@@ -28,30 +28,45 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => 'Greatleaf Land Inc.',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
-    ];
+     
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
     } else {
+        $menuItems[] = ['label' => 'My Profile', 'url' => ['/site/signup']];
+        $menuItems[] = ['label' => 'Daily Track Record', 'url' => ['/site/login']];
+        
+        
+
         $menuItems[] = [
             'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
             'url' => ['/site/logout'],
             'linkOptions' => ['data-method' => 'post']
-        ];
+        ];   
     }
+
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => $menuItems,
+    ]);
+
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => [
+                        ['label' => 'Submit Requests', 'items' => [
+                        ['label' => 'Overtime', 'url' => '#'],
+                        ['label' => 'Leave', 'url' => '#'],
+                        ['label' => 'Cash Voucher', 'url' => '#'],
+                        ['label' => 'Job Accomplishment', 'url' => '#'],
+                        ['label' => 'Official Business', 'url' => '#'],
+                        ]],
+                    ],
     ]);
     NavBar::end();
     ?>
@@ -67,9 +82,8 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-right">&copy; Greatleaf Land Inc. <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
 
